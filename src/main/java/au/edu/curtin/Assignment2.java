e7802f084817fe7380f7e863b9edc9cd3959e2c9
Amila Kumara <amilarajans@gmail.com>
2017-05-14 01:33:10 +0530

    code cleanup

--- src/main/java/au/edu/curtin/Assignment2.java
+++ src/main/java/au/edu/curtin/Assignment2.java
@@ -13,17 +13,19 @@ public class Assignment2 {
 	public static int totalClass = 1;
 
 	public static void main(String[] args) {
-		args = new String[3];
-		args[0] = "D:\\dev\\Projects\\SoftwareMetrics\\src\\main\\java\\au\\edu\\curtin\\";
-		args[1] = "A.class";
-		args[2] = "";
+		if(args.length==0){
+			args = new String[3];
+			args[0] = "D:\\dev\\Projects\\SoftwareMetrics\\src\\main\\java\\au\\edu\\curtin\\";
+			args[1] = "A.class";
+			args[2] = "";
+		}
 
 		if (args.length >= 2) {
 			new Program(args[0], args[1], args[2], true);
 
-			System.out.println("Total number of classes involved    ----" + totalClass);
-			System.out.println("Total number of unique constructors ----" + totalConstructors);
-			System.out.println("Total number of unique methods      ----" + totalMethods);
+			System.out.println("Total number of classes involved    ---- " + totalClass);
+			System.out.println("Total number of unique constructors ---- " + totalConstructors);
+			System.out.println("Total number of unique methods      ---- " + totalMethods);
 		} else {
 			System.out.println("Usage:");
 			System.out.println("\tAssignment2 <Base Path> <Class Name> [Method Name]");
